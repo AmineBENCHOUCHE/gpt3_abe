8b6a40d29e0bcf1f1ba09e5ee2cff3ca4e2d648b
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2023-09-06 18:35:59 +0200

    Add Cta Section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,16 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className='gpt3__cta section__padding' >
+      <div className='gpt3__cta_register'>
+        <p>
+          Request Early Access to Get Started
+        </p>
+        <h3>Register today & start exploring the endless posssibilities</h3>
+
+      </div>
+      <button type="button">Get Started</button>
+      
     </div>
   )
 }
