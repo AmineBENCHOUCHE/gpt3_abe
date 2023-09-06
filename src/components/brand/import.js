3c1dab51b6de0f1578232265dbd1e3bf52bbeded
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2023-09-06 13:19:02 +0200

    Add Brand component and create file to import img

---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from "../../assets/google.png"
+import slack from "../../assets/slack.png"
+import shopify from "../../assets/shopify.png"
+import dropbox from "../../assets/dropbox.png"
+import atlassian from "../../assets/google.png"
+
+export{
+    google,
+    slack ,
+    shopify ,
+    dropbox ,
+    atlassian
+}
