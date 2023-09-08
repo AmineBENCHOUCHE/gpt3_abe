39efd989d256456aa48e27e7dd8b2ffc3b3d98dc
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2023-09-08 18:15:13 +0200

    Add import file for the images and add component blog and article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
+
+
+const Article = ({date, title, subtitle, image}) => {
+  
   return (
-    <div>
-      Article
-    </div>
+        <div className='gpt3__article'>
+          <img src={image} alt="" />
+          <div className='gpt3__article-description'>
+            <h5>{date}</h5>
+            <h4>{title}</h4>
+            <h5>{subtitle="Read Full Article"}</h5>
+          </div>
+        </div>
   )
+
 }
 
 export default Article
--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import './blog.css'
+import { Article } from '../../components'
+import {blog01, blog02,blog03,blog04,blog05} from "./import"
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className='gpt3__blog section__padding'> 
+      <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      <div className="gpt3__blog_images">
+        <div className='gpt3__blog_images_image1'> 
+          <Article  title={"GPT-3 and Open AI is the future. Let us explore how it is?"} date={"30 September 2021"} image={blog01}/>
+        </div>
+        <div className='gpt3__blog_images_image2'>
+          <Article  title={"GPT-3 and Open AI is the future. Let us explore how it is?"} date={"30 September 2021"} image={blog02}/>
+        </div>
+
+        <div className='gpt3__blog_images_image3'>
+          <Article  title={"GPT-3 and Open AI is the future. Let us explore how it is?"} date={"30 September 2021"} image={blog03}/>
+        </div>
+        <div className='gpt3__blog_images_image4'>
+          <Article  title={"GPT-3 and Open AI is the future. Let us explore how it is?"} date={"30 September 2021"} image={blog04}/>
+        </div >
+        <div className='gpt3__blog_images_image5'>
+          <Article  title={"GPT-3 and Open AI is the future. Let us explore how it is?"} date={"30 September 2021"} image={blog05}/>
+        </div>
+        
+      </div>
     </div>
   )
 }
