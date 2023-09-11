7a9b7d1e0a33dc3dff1455c02cc66988f0ed2e34
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2023-09-11 17:22:01 +0200

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
         <Possibility />
         <CTA />
         <Blog />
+        <Footer />
 
       </div>
   )
--- src/container/footer/Footer.jsx
+++ src/container/footer/Footer.jsx
@@ -3,8 +3,11 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='gpt3__footer section__padding'>
+      <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+      <div className='gpt3__footer-button'>
+        <button type="button">Request Early Access</button>
+        </div>
     </div>
   )
 }
