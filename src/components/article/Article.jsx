eb76d3a4949dc4860504aeb89cc38ec116571179
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2023-09-11 16:49:44 +0200

    Add Article Component and Blog Component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -7,12 +7,12 @@ const Article = ({date, title, subtitle, image}) => {
   
   return (
         <div className='gpt3__article'>
-          <img src={image} alt="" />
+          <img src={image} alt="blog" />
           <div className='gpt3__article-description'>
             <h5>{date}</h5>
-            <h4>{title}</h4>
-            <h5>{subtitle="Read Full Article"}</h5>
+            <h3>{title}</h3>
           </div>
+            <p>{subtitle="Read Full Article"}</p>
         </div>
   )
 
