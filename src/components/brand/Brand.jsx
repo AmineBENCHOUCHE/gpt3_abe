366e8fb46f0d1310533303838a847ec7b93686df
Amine BENCHOUCHE <benchouche.amine@outlook.com>
2023-09-12 14:50:51 +0200

    Update Brand component with animation

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -5,11 +5,13 @@ import { google, slack, shopify, atlassian, dropbox } from "./import"
 const Brand = () => {
   return (
     <div className='gpt3__brand section__padding'>
-      <div><img src={google} alt='google' /></div>
-      <div><img src={slack} alt="slack" /></div>
-      <div><img src={shopify} alt="shopify" /></div>
-      <div><img src={dropbox} alt="dropbox" /></div>
-      <div><img src={atlassian} alt="atlassian" /></div>
+      <div className="gpt3__brand-container">
+        <div><img src={google} alt='google' /></div>
+        <div><img src={slack} alt="slack" /></div>
+        <div><img src={shopify} alt="shopify" /></div>
+        <div><img src={dropbox} alt="dropbox" /></div>
+        <div><img src={atlassian} alt="atlassian" /></div>
+      </div>
     </div>
   )
 }
